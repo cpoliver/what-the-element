5bfccd703298b7e1d091944cb2fa490a112e0513
Charles Oliver <cpoliver@gmail.com>
2017-05-16 23:53:55 +0200

    Add search box component and functionality

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,19 +1,44 @@
 import * as React from 'react';
 
 import './app.css';
+import { ElementType } from './element';
 import { ElementGrid } from './element-grid';
+import { SearchBox } from './search-box';
 
 const logo = require('../../public/logo.svg');
-const pt = require('periodic-table');
+const elements = require('periodic-table').all();
 
-class App extends React.Component<{}, null> {
+const isMatch = (searchText: string, elementName: string) => (
+  new RegExp(`^${searchText}`, 'i').test(elementName)
+);
+
+class App extends React.Component<{}, any> {
+  constructor(props: {}) {
+    super(props);
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+
+    this.state = { elements, searchText: '' };
+  }
+
+  onSearchTextChange(e: React.FormEvent<HTMLInputElement>) {
+    const searchText = (e.target as HTMLInputElement).value;
+
+    this.setState({
+      searchText,
+      elements: elements.filter(
+        (element: ElementType) => isMatch(searchText, element.name)
+      )
+    });
+  }
+  
   render() {
     return (
       <div className="app">
         <img src={logo} className="app-logo" alt="logo" />
         <h2 className="title">What the Element?</h2>
         <span className="subtitle">testing typescript with create-react-app&hellip; for science!</span>
-        <ElementGrid elements={pt.all()} />
+        <SearchBox text={this.state.text} onChangeHandler={this.onSearchTextChange} />
+        <ElementGrid elements={this.state.elements} />
       </div>
     );
   }
---
+++ src/components/search-box/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import './search-box.css';
+
+export type SearchBoxType = {
+  text?: string,
+  onChangeHandler: Function
+};
+
+export const SearchBox: React.StatelessComponent<SearchBoxType> = ({ text = '', onChangeHandler }) => (
+  <div className="search-box">
+    <input placeholder="search"
+           onChange={(e: React.FormEvent<HTMLInputElement>): void => onChangeHandler(e)} />
+  </div>
+);
